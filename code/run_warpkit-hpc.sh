a19c46efdd89ea80f50fc12f3e891455e81cffc8
Melanie Kos <tur61139@login1.hpc>
2025-11-13 02:26:17 -0500

    commit for all rf1 data using bids session subfolders. all necessary hpc preprocessing codes, helper codes/files, and select outputs: addIntendedFor, fmriprep24/25, tedana24/25, genTedanaconfounds and outputs

---
+++ code/run_warpkit-hpc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ensure paths are correct
+maindir=/gpfs/scratch/tug87422/smithlab-shared/rf1-sra-linux2 #this should be the only line that has to change if the rest of the script is set up correctly
+scriptdir=$maindir/code
+
+
+#mapfile -t myArray < ${scriptdir}/sublist_225.txt
+mapfile -t myArray < ${scriptdir}/sublist_new.txt
+
+# grab the first ntasks elements
+ntasks=1
+counter=0
+while [ $counter -lt ${#myArray[@]} ]; do
+	subjects=${myArray[@]:$counter:$ntasks}
+	echo $subjects
+	let counter=$counter+$ntasks
+	qsub -v subjects="${subjects[@]}" warpkit-hpc.sh
+done
