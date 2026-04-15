cc10abb80f238fbc9e496d034b0d82b644c4dc5e
David V. Smith <david.v.smith@temple.edu>
2026-04-14 22:31:22 -0400

    initial commit

---
+++ code/fmriprep.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# example code for FMRIPREP
+# runs FMRIPREP on input subject
+# usage: bash run_fmriprep.sh sub
+# example: bash run_fmriprep.sh 102
+
+sub=$1
+
+# ensure paths are correct irrespective of where the user runs the script
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+maindir="$(dirname "$scriptdir")"
+
+# host-system paths
+bidsdir="$maindir/bids"
+derivdir="$maindir/derivatives"
+scratchdir=/ZPOOL/data/scratch/$(whoami)
+templateflow_host=/ZPOOL/data/tools/templateflow
+mplconfig_host=/ZPOOL/data/tools/mplconfigdir
+licenses_host=/ZPOOL/data/tools/licenses
+fmriprep_img=/ZPOOL/data/tools/fmriprep-25.2.4.simg
+
+# container paths
+base_container=/base
+templateflow_container=/opt/templateflow
+mplconfig_container=/opt/mplconfigdir
+licenses_container=/opts
+scratch_container=/scratch
+
+# make derivatives folder if it doesn't exist
+if [ ! -d "$derivdir" ]; then
+    mkdir -p "$derivdir"
+fi
+
+# make scratch folder if it doesn't exist
+if [ ! -d "$scratchdir" ]; then
+    mkdir -p "$scratchdir"
+fi
+
+export SINGULARITYENV_TEMPLATEFLOW_HOME=$templateflow_container
+export SINGULARITYENV_MPLCONFIGDIR=$mplconfig_container
+
+singularity run --cleanenv \
+    -B ${templateflow_host}:${templateflow_container} \
+    -B ${mplconfig_host}:${mplconfig_container} \
+    -B ${maindir}:${base_container} \
+    -B ${licenses_host}:${licenses_container} \
+    -B ${scratchdir}:${scratch_container} \
+    ${fmriprep_img} \
+    ${base_container}/bids ${base_container}/derivatives/fmriprep \
+    participant --participant_label $sub \
+    --stop-on-first-crash \
+    --me-output-echos \
+    --output-spaces MNI152NLin6Asym \
+    --bids-filter-file ${base_container}/code/fmriprep_config.json \
+    --fs-no-reconall \
+    --fs-license-file ${licenses_container}/fs_license.txt \
+    -w ${scratch_container}
